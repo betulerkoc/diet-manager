00a8782f72bb12704d47adb13a5019a4af325aec
betul <bet.er@hotmail.com>
2020-10-08 20:05:25 +0300

    fixed firebase issue

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
-function Menu(props) {
+function Board(props) {
 
   return (
     <div className="card" style={{ width: '18rem' }}>
-        {console.log(props)}
+        {console.log(props.data.myMenu)}
         <h1>Menu</h1>
-        <div><h3>Breakfast:</h3> {props.data.breakfast.map(b =>  <span><br/> {b.mealName}, {b.calories} <br/></span>)}</div>
-        <div><h3>Lunch</h3> <span> {props.data.lunch.map(b =>  <span><br/> {b.mealName}, {b.calories} <br/></span>)} </span> </div>
-        <div><h3>Middle Lunch:</h3> <span> {props.data.middleLunch.map(b =>  <span><br/> {b.mealName}, {b.calories} <br/></span>)}  </span> </div>
-        <div><h3>Dinner: </h3> <span>{props.data.dinner.map(b =>  <span><br/> {b.mealName}, {b.calories} <br/></span>)}  </span> </div>
-        <div><h3>Middle Dinner:</h3> <span>{props.data.middleDinner.map(b =>  <span><br/> {b.mealName}, {b.calories} <br/></span>)}  </span> </div>
+
+
+        <div><h3>Breakfast:</h3> {props.data.myMenu.breakfast.map(b =>  <span><br/> {b.mealName}, {b.calories} <br/></span>)}</div>
+        <div><h3>Lunch</h3> <span> {props.data.myMenu.lunch.map(b =>  <span><br/> {b.mealName}, {b.calories} <br/></span>)} </span> </div>
+        {/* <div><h3>Middle Lunch:</h3> <span> {props.data.myMenu.middleLunch.map(b =>  <span><br/> {b.mealName}, {b.calories} <br/></span>)}  </span> </div> */}
+        <div><h3>Dinner: </h3> <span>{props.data.myMenu.dinner.map(b =>  <span><br/> {b.mealName}, {b.calories} <br/></span>)}  </span> </div>
+        {/* <div><h3>Middle Dinner:</h3> <span>{props.data.myMenu.middleDinner.map(b =>  <span><br/> {b.mealName}, {b.calories} <br/></span>)}  </span> </div> */}
     </div>
   );
 }
 
-export default Menu;
+export default Board;
